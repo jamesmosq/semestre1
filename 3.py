05d37cd7179efe924d88a80b05bcee526e277bd7
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-02-23 19:45:34 -0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+# Ejercicio 3: Duplicar Elementos de una Lista
+
+frutas = ["manzana", "plátano", "uva", "naranja"]
+frutas_duplicadas = [fruta * 2 for fruta in frutas]
+
+print(f"Lista original: {frutas}")
+print(f"Lista con elementos duplicados: {frutas_duplicadas}")
